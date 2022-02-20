49d39937cedd5f55703f659bb492e7eacff2baa8
Muhammad Nur Ubaidillah <nur.ubaid27@gmail.com>
2022-02-20 19:07:17 +0700

    update

--- script/slider.js
+++ script/slider.js
@@ -24,3 +24,21 @@ $(document).ready(function () {
           }
      })
 });
+
+// Testimonial
+$(document).ready(function () {
+     $('.testimonials').slick({
+          autoplay: true,
+          infinite: true,
+          speed: 400,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          responsive: [{
+               breakpoint: 992,
+               settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+               }
+          }]
+     })
+});
